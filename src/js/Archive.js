a87d07d3043c52842385281d32f9353ae1c6994f
Luke <kruyerl@gmail.com>
2019-04-28 23:09:48 +0200

    first commit

---
+++ src/js/Archive.js
@@ -0,0 +1,14 @@
+/** ===============================
+ * Date:                28/04/19
+ * Problem Statement:   'We have defined a function, filteredArray, which takes arr, a nested array, and elem as arguments, and returns a new array. elem represents an element that may or may not be present on one or more of the arrays nested within arr. Modify the function, using a for loop, to return a filtered version of the passed array such that any array nested within arr containing elem has been removed.'
+ * Time:                00:00
+ * Code:
+ *      <
+ *
+ *      />
+ * AfterActionReview: {
+ *      Effective:      ''
+ *      Ineffective:    ''
+ *      Improvements:   ''
+ * }
+ =============================== */
